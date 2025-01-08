e6dcb7f20f6dfa4de7454f8316475f7ab0776296
ish2k <ishaa.gupta04@hotmail.com>
2025-01-08 20:33:59 +0530

    worflow and readme modified

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Stock Picking System
-Stock-Picking System Based on Warren Buffett's Method
+
+This Stock Picking Method is inspired from the book "The New Tao of Warren Buffet". This book explains the 
+mindset of the famous billionaire while making investment decisions. He stresses on the Earnings Per 
+Share (EPS), current price of the Company and finally, future growth prospects. 
+
+
--- stock_picking/top_stocks.py
+++ stock_picking/top_stocks.py
@@ -3,7 +3,7 @@ from stock_picking.piotroski_score import calculate_piotroski_score
 from stock_picking.fetch_data import fetch_financial_data
 
 def get_top_stocks(
-        tickers: str | list[str],
+        tickers: str | pd.DataFrame | list[str],
         exchange: str = 'NYSE', 
         stock_return_threshold : float = 0.045,
         piotroski_score_threshold : int = 7,
@@ -17,24 +17,29 @@ def get_top_stocks(
     Fetch the top stocks based on the Piotroski Score and EPS / Last Price.
 
     Parameters:
-    tickers (str or list): If a string, it should be a path to csv file containing "Symbol" column.
+    tickers (str | Dataframe | list): 
+        If a string, it should be a path to csv file containing "Symbol" column.
+        If a DataFrame, it should contain a "Symbol" column.
         If a list, it should contain stock ticker symbols.
     exchange (str): The stock exchange to fetch the data from - It can only be ['NYSE', 'NASDAQ', 'NSE', 'BSE'].
     stock_return_threshold (float): The threshold for the EPS / Last Price ratio (risk free return).
     piotroski_score_threshold (int): The minimum Piotroski Score required for a stock to be considered.
     market_cap_threshold (float): The minimum market capitalization required for a stock to be considered.
-    sort_by (str or list): The column(s) to sort the top stocks by.
-    sort_precedence (bool or list): The sort order for the columns specified in sort_by.
+    sort_by (str | list): The column(s) to sort the top stocks by.
+    sort_precedence (bool | list): The sort order for the columns specified in sort_by.
     generate_csv (bool): If True, a CSV file containing the top stocks will be generated. 
     file_path (str): The path to the CSV file where the top stocks will be saved.  
 
     Returns:
-    str or list: If a string is provided as input, the function returns the stock ticker symbol of the top stock.
+    str | list: If a string is provided as input, the function returns the stock ticker symbol of the top stock.
         If a list is provided, the function returns a list of stock ticker symbols of the top stocks.
     """
     
     if isinstance(tickers, str):
         tickers = pd.read_csv(tickers)
+        tickers = tickers['Symbol'].tolist()
+    
+    elif isinstance(tickers, pd.DataFrame):
         tickers = tickers['Symbol'].tolist()
 
     tickers = list(set(tickers))
